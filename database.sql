91c3c09086d946ff148846ee9323eba3e41fc81d
Webbly <rackyy55@gmail.com>
2021-10-28 16:38:57 -0400

    cleanup database

--- database.sql
+++ database.sql
@@ -320,24 +320,17 @@ INSERT INTO `record` VALUES (10001,1004,'2018-04-16','hththjtyjtyjtyjt','dfgtrhr
 /*!40000 ALTER TABLE `record` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `room`
---
+
 
 DROP TABLE IF EXISTS `room`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `room` (
   `id` int(11) NOT NULL,
   `type` varchar(30) DEFAULT NULL,
   `patients` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `room`
---
 
 LOCK TABLES `room` WRITE;
 /*!40000 ALTER TABLE `room` DISABLE KEYS */;
