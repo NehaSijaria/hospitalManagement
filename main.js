9db00c338999619b840c8020e82435deb2eef85e
Webbly <rackyy55@gmail.com>
2021-10-28 16:59:12 -0400

    add main.js

---
+++ main.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var session = require('express-session');
+var path = require('path');
+var login = require('./controllers/login_controller');
+
+var app = express();
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+
+
+
+// set up template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//static files
+
+app.use(express.static('./public')); 
+
+// Parser
+
+app.use(cookieParser());
+
+// Express Session
+
+app.use(session({
+	secret: 'keyboard cat',
+	key: 'user', 
+	cookie: { maxAge: 3600000, secure: false },
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Passport Init
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validation
+
+app.use(expressValidator());
+  
+
+// Connect Flash
+
+app.use(flash());
+
+// fire controllers
+
+
+login(app);
+
+
+function ensureAuthenticated(req, res, next){
+		if(req.isAuthenticated()){
+			// AUTHENTICATED ==> continue
+			User.getUserTypeById(req.session.passport.user, function(err, result){
+				if(err) throw err;
+				
+					return next();
+				
+				
+			});
+		}
+		else{
+			// NOT AUTHENTICATED ==> goto login page
+			res.redirect('/login');
+		}
+	}
+
+
+app.listen(3000);
